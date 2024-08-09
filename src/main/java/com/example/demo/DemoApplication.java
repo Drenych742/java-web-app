aa7b75e65cc3a8acbdf67570b6c9f6e8277f7a02
Drenych742 <65564940+Drenych742@users.noreply.github.com>
2024-08-09 17:50:21 +0300

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,14 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-
+;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@RequestMapping("/")
   public String home() {
-	  ;
     return "Hello Docker World!";
 	  
   }
